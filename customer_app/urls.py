1da23593adfa6ab666ad508c3a15696ed4e33131
Aditya Singh <adityas7898@gmail.com>
2023-12-15 18:59:55 +0530

    update

--- customer_app/urls.py
+++ customer_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from customer_app.views import customer_register_view,customer_list_view,customer_login_view,customer_home_view,customer_logout_view,forgot_pwd_view,change_pwd_view,customer_otp_view,login_demo_view
+from customer_app.views import customer_register_view,customer_list_view,customer_login_view,customer_home_view,customer_logout_view,forgot_pwd_view,change_pwd_view,customer_otp_view,login_demo_view, cust_category_list_view,pro_item_list_view
 
 app_name = 'customer_app'
 urlpatterns=[
     path(route='customer_register/',view=customer_register_view,name='customer_register'),
     path(route='customer_list/',view=customer_list_view,name='customer_list'),
+    path(route='cust_category_list/',view=cust_category_list_view,name="cust_category_list"),
+    path(route='pro_item_list/',view=pro_item_list_view,name="pro_item_list"),
+    
     path(route='customer_login/',view=customer_login_view,name='customer_login'),
     path(route='customer_logout/',view=customer_logout_view,name='customer_logout'),
     path(route='customer_home/',view=customer_home_view,name='customer_home'),
