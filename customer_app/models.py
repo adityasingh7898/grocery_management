496be502602472452e12708366f0bc54db48252d
Aditya Singh <adityas7898@gmail.com>
2023-12-21 14:53:38 +0530

    update

--- customer_app/models.py
+++ customer_app/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class customer_model(User):
-
     phone = models.PositiveBigIntegerField(unique=True)
     dob = models.DateField()
     gender = models.CharField(max_length=10,choices=[['Male','Male'],['Female','Female'],['other','other']])
